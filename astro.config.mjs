a5502d8da86647924b6b8afe12eb3acb10b81fbb
Shankar Chandrasekar <shankar.chandrasekar@nortonrosefulbright.com>
2025-12-02 16:59:32 +0000

    latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 import vercel from "@astrojs/vercel";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
